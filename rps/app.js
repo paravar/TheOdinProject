c4d25244253e323066d4990e4acd40e193945cdd
Pedrum <p.paravar@gmail.com>
2022-09-24 14:05:57 +0330

    first working prototype

--- rps/app.js
+++ rps/app.js
@@ -1,3 +1,42 @@
+let computerChoice;
+let playerChoice;
+let computerScore = 0;
+let playerScore = 0;
+let announce = "⬇️Choose to start!⬇️";
+
+let announcement = document.querySelector("#announce");
+announcement.textContent = `⬇️  Choose to start!  ⬇️`;
+
+let player = document.querySelector("#human");
+player.textContent = `You: ${playerScore}`;
+
+let computer = document.querySelector("#computer");
+computer.textContent = `Computer: ${computerScore}`;
+
+let button = document.querySelectorAll("button");
+button.forEach((b) => b.addEventListener("click", pp));
+
+function pp() {
+  playerChoice = this.id;
+  computerChoice = getComputerChioce();
+  let result = playRound(playerChoice, computerChoice);
+  updateScore(result);
+}
+
+function updateScore(whoWon) {
+  if (whoWon === "tie") {
+    announcement.textContent = `Both chose: ${playerChoice} - It's a Tie!`;
+  } else if (whoWon === "computer won") {
+    computerScore++;
+    computer.textContent = `Computer: ${computerScore}`;
+    announcement.textContent = `Computer won`;
+  } else {
+    playerScore++;
+    player.textContent = `You: ${playerScore}`;
+    announcement.textContent = `You won!`;
+  }
+}
+
 function getComputerChioce() {
   let choices = ["rock", "paper", "scissors"];
   let choiceNumber = Math.floor(Math.random() * 3);
@@ -18,34 +57,3 @@ function playRound(playerSelection, computerSelection) {
     return "player won";
   }
 }
-
-/* function play5Rounds() {
-  let computerScore = 0;
-  let playerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    playerChoice = prompt("Enter your choise [rock / paper / scissors]");
-    computerChoice = getComputerChioce();
-
-    switch (playRound(playerChoice, computerChoice)) {
-      case "tie":
-        break;
-      case "computer won":
-        computerScore++;
-        break;
-      case "player won":
-        playerScore++;
-        break;
-      default:
-        console.log("Something went wront!");
-    }
-  }
-  if (computerScore === playerScore) {
-    console.log("It's a tie");
-  } else if (computerScore > playerScore) {
-    console.log("Computer Won!");
-  } else {
-    console.log("Player Won!");
-  }
-}
-
-play5Rounds(); */
